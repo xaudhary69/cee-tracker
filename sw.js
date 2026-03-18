e2e9d7d168757d10859ab3162b67da32a1f0ebba
xaudhary69 <nepaligenz01@gmail.com>
2026-03-18 22:27:15 +0545

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='cee-v3';
+const ASSETS=['./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
